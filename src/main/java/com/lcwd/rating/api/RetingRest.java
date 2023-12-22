98d366e5fbe29b31be61895b0a39d98bff3b8d3c
Doke Bapurao <doke.bapurao@mphasis.com>
2023-12-22 14:34:25 +0530

    final commit

--- src/main/java/com/lcwd/rating/api/RetingRest.java
+++ src/main/java/com/lcwd/rating/api/RetingRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ public class RetingRest {
 	@Autowired
 	private RatingService ratingService;
 
+	@PreAuthorize("hasAuthority('Admin') ")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Rating> create(@RequestBody Rating rating) {
@@ -36,6 +38,7 @@ public class RetingRest {
 
 	}
 
+	@PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin') ")
 	@GetMapping("/user/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<List<Rating>> getRatingByUserId(@PathVariable("id") String userId) {
--- src/main/java/com/lcwd/rating/entity/Rating.java
+++ src/main/java/com/lcwd/rating/entity/Rating.java
@@ -1,8 +1,9 @@
 package com.lcwd.rating.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
